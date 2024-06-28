3e2f2854c5046a0cdfbb26ddf944f14cb3dd188d
Animesh Raj <animesh080808@gmail.com>
2024-06-29 02:24:03 +0530

    Updated userprofile, version upgrade 2.2.0 -> 2.3.0
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- backend/controllers/userControllers/updateUserProfile.js
+++ backend/controllers/userControllers/updateUserProfile.js
@@ -6,28 +6,28 @@ import asyncHandler from "express-async-handler";
 // @route: 	PUT api/users/profile
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
+	const user = await User.findById(req.user._id);
 
-  if (user) {
-    const { name, email, password } = req.body;
-    user.name = name || user.name;
-    user.email = email || user.password;
-    if (password) user.password = password;
+	if (user) {
+		const { name, email, password } = req.body;
+		user.name = name || user.name;
+		user.email = email || user.password;
+		if (password) user.password = password;
 
 
-    await user.save();
+		await user.save();
 
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    });
-  } else {
-    const message = "User is unavailable";
-    res.status(404).json({ message });
-    throw new Error(message);
-  }
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		});
+	} else {
+		const message = "User is unavailable";
+		res.status(404).json({ message });
+		throw new Error(message);
+	}
 });
 
 export default updateUserProfile;
